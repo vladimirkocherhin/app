e4fa28bf5d346a340e983b007632e92a423b8b02
vladimirkocherhin <vllad.ko@gmail.com>
2020-05-09 14:19:15 +0200

    some improvements

--- src/AppBundle/Controller/HomepageController.php
+++ src/AppBundle/Controller/HomepageController.php
@@ -5,39 +5,19 @@ namespace AppBundle\Controller;
 
 
 use eZ\Bundle\EzPublishCoreBundle\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use eZ\Publish\API\Repository\ContentService;
-use eZ\Publish\API\Repository\LocationService;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class HomepageController extends Controller
 {
-    private $contentService;
-
-    private $locationService;
-
-    public function __construct(ContentService $contentService, LocationService $locationService)
-    {
-        $this->contentService = $contentService;
-        $this->locationService = $locationService;
-    }
-
     /**
      * @param Request $request
-     * @return JsonResponse
-     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
-     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     * @return Response
      */
     public function fullViewAction(Request $request)
     {
-        $rootLocationId = $this->getConfigResolver()->getParameter('content.tree_root.location_id');
-        $rootLocation = $this->locationService->loadLocation($rootLocationId);
-        dump($rootLocation); die();
-
-        $response = new JsonResponse();
-        $response->setData(['data' => 123]);
-
-        return $response;
+        $response = new Response();
+        return $this->render('/full/home_page.html.twig');
     }
 }
